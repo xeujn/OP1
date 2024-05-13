af866e8ce0151c1c3428966013c38e10fd6b8342
xeujn <94025386+xeujn@users.noreply.github.com>
2024-05-13 12:52:15 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#修改默认主题1
+#修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
